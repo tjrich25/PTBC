906624bd7a656f8fd69ca9f196b7290982a423d7
Travis <Travis@Traviss-MacBook-Pro.local>
2016-10-15 10:38:05 -0400

    added if/else to include comma, spaces, and 'and'

--- weekTwoReview/script.js
+++ weekTwoReview/script.js
@@ -8,9 +8,15 @@
 
 function makePizza(chosenCrust, chosenToppings) {
   var toppings = "";
-  chosenToppings.forEach(function(topping) {
+  chosenToppings.forEach(function(topping, index, arr) {
+    if (index === arr.length-1) {
     toppings += topping;
+  } else if (index === arr.length-2) {
+      toppings += topping + ", and ";
+  } else {
+    toppings += topping + ", ";
+  }
   });
   console.log("Here's your " + toppings +  " pizza with a " + chosenCrust + " crust!");
 }
-makePizza("butter cheese", ["pepperoni ", "bacon ", "banana peppers ", "anchovies ", "pineapple"]);
+makePizza("butter cheese", ["pepperoni", "black olives", "bacon", "banana peppers", "anchovies", "pineapple"]);
