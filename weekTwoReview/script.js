14d3c819b6bf33e0c0078315d03c1024c1454cde
Travis <Travis@Traviss-MacBook-Pro.local>
2016-10-15 11:17:32 -0400

    added function with objects

--- weekTwoReview/script.js
+++ weekTwoReview/script.js
@@ -6,9 +6,26 @@
 //
 // writeStatement();
 
-function makePizza(chosenCrust, chosenToppings) {
+// function makePizza(chosenCrust, chosenToppings) {
+//   var toppings = "";
+//   chosenToppings.forEach(function(topping, index, arr) {
+//     if (index === arr.length-1) {
+//     toppings += topping;
+//   } else if (index === arr.length-2) {
+//       toppings += topping + ", and ";
+//   } else {
+//     toppings += topping + ", ";
+//   }
+//   });
+//   console.log("Here's your " + toppings +  " pizza with a " + chosenCrust + " crust!");
+// }
+
+// makePizza("butter cheese", ["pepperoni", "black olives", "bacon", "banana peppers", "anchovies", "pineapple"]);
+
+function makePizza(groupOrder) {
+  groupOrder.forEach(function(individualOrder) {
   var toppings = "";
-  chosenToppings.forEach(function(topping, index, arr) {
+  individualOrder.toppings.forEach(function(topping, index, arr) {
     if (index === arr.length-1) {
     toppings += topping;
   } else if (index === arr.length-2) {
@@ -17,6 +34,32 @@ function makePizza(chosenCrust, chosenToppings) {
     toppings += topping + ", ";
   }
   });
-  console.log("Here's your " + toppings +  " pizza with a " + chosenCrust + " crust!");
+  console.log(individualOrder.name + " ordered a " + toppings +
+  " pizza with a " + individualOrder.crust + " crust!");
+});
 }
-makePizza("butter cheese", ["pepperoni", "black olives", "bacon", "banana peppers", "anchovies", "pineapple"]);
+
+var ourOrders = [
+  {
+    name: "Aisha",
+    crust: "cheesy pretzel",
+    toppings: ["goat cheese", "figs", "diced tomatoes"]
+  },
+  {
+    name: "Travis",
+    crust: "parmesan",
+    toppings: ["pepperoni", "bacon"]
+  },
+  {
+    name: "Deleana",
+    crust: "garlic cheese",
+    toppings: ["anchovies", "black olives"]
+  },
+  {
+    name: "Gaoio",
+    crust: "cajun",
+    toppings: ["pepperoni", "bacon"]
+  }
+];
+
+makePizza(ourOrders);
