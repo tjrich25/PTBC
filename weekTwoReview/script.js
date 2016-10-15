b5f4a820cbc76195585eff3123b31607b5709f9c
Travis <Travis@Traviss-MacBook-Pro.local>
2016-10-15 14:08:36 -0400

    for loop

--- weekTwoReview/script.js
+++ weekTwoReview/script.js
@@ -64,7 +64,17 @@
 //
 // makePizza(ourOrders);
 
-for (var i = 0; i < 100; i++) {
-  var numberOne=0;
-  var numberTwo=1;
+var numberOne=0;
+var numberTwo=1;
+for (var i = 0; i < 100; i++ ) {
+  if (i === 0) {
+    console.log(numberOne);
+  } else if (i === 1) {
+    console.log(numberTwo)
+  } else {
+  var result = numberOne + numberTwo;
+  numberOne = numberTwo;
+  numberTwo = result;
+  console.log(result);
+  }
 }
